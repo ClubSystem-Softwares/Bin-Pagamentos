b81d64584cc919bb46e84602a9e52e0478466ad6
Matheus Lopes Santos <fale_com_lopez@hotmail.com>
2020-09-22 17:05:37 -0300

    Inclusão de getters e setters para os atributos da classe Criação de testes mais concretos sobre a classe Environment

--- src/Environment.php
+++ src/Environment.php
@@ -2,6 +2,8 @@
 
 namespace CSWeb\BIN;
 
+use InvalidArgumentException;
+
 /**
  * Environment
  *
@@ -11,7 +13,110 @@ namespace CSWeb\BIN;
  */
 class Environment
 {
-    public function __construct()
+    /**
+     * @var string
+     */
+    protected $username;
+
+    /**
+     * @var string
+     */
+    protected $password;
+
+    /**
+     * @var string
+     */
+    protected $sslCert;
+
+    /**
+     * @var string
+     */
+    protected $sslKey;
+
+    /**
+     * @var string
+     */
+    protected $sslPassword;
+
+    public function __construct(
+        string $username,
+        string $password,
+        string $sslCert,
+        string $sslKey,
+        string $sslPassword
+    ) {
+        $this->setUsername($username);
+        $this->setPassword($password);
+        $this->setSslCert($sslCert);
+        $this->setSslKey($sslKey);
+        $this->setSslPassword($sslPassword);
+    }
+
+    public function getUsername() : string
     {
+        return $this->username;
+    }
+
+    public function setUsername(string $username) : Environment
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getPassword() : string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password) : Environment
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getSslCert() : string
+    {
+        return $this->sslCert;
+    }
+
+    public function setSslCert(string $sslCert) : Environment
+    {
+        if (!is_file($sslCert)) {
+            throw new InvalidArgumentException('SSL cert is not a file');
+        }
+
+        $this->sslCert = $sslCert;
+
+        return $this;
+    }
+
+    public function getSslKey() : string
+    {
+        return $this->sslKey;
+    }
+
+    public function setSslKey(string $sslKey) : Environment
+    {
+        if (!is_file($sslKey)) {
+            throw new InvalidArgumentException('SSL key is not a file');
+        }
+
+        $this->sslKey = $sslKey;
+
+        return $this;
+    }
+
+    public function getSslPassword() : string
+    {
+        return $this->sslPassword;
+    }
+
+    public function setSslPassword(string $sslPassword) : Environment
+    {
+        $this->sslPassword = $sslPassword;
+
+        return $this;
     }
 }
--- tests/EnvironmentTest.php
+++ tests/EnvironmentTest.php
@@ -9,6 +9,68 @@ class EnvironmentTest extends TestCase
 {
     public function testInstance()
     {
-        $this->assertInstanceOf(Environment::class, new Environment());
+        $username = $this->username();
+        $password = $this->password();
+        $sslPass  = $this->sslPassword();
+
+        $sslCert = __DIR__ . '/fixtures/certs/private_key.pem';
+        $sslKey  = __DIR__ . '/fixtures/certs/server.csr';
+
+        $environment = new Environment($username, $password, $sslCert, $sslKey, $sslPass);
+
+        $this->assertInstanceOf(Environment::class, $environment);
+        $this->assertEquals($username, $environment->getUsername());
+        $this->assertEquals($password, $environment->getPassword());
+        $this->assertEquals($sslPass, $environment->getSslPassword());
+        $this->assertEquals($sslCert, $environment->getSslCert());
+        $this->assertEquals($sslKey, $environment->getSslKey());
+    }
+
+    public function testInvalidCertFile()
+    {
+        $this->expectExceptionMessage('SSL cert is not a file');
+
+        $username = $this->username();
+        $password = $this->password();
+        $sslPass  = $this->sslPassword();
+
+        $sslCert = __DIR__ . '/fixtures/certs/invalid_file.pem';
+        $sslKey  = __DIR__ . '/fixtures/certs/server.csr';
+
+        $environment = new Environment($username, $password, $sslCert, $sslKey, $sslPass);
+    }
+
+    public function testInvalidKeyFile()
+    {
+        $this->expectExceptionMessage('SSL key is not a file');
+
+        $username = $this->username();
+        $password = $this->password();
+        $sslPass  = $this->sslPassword();
+
+        $sslCert = __DIR__ . '/fixtures/certs/private_key.pem';
+        $sslKey  = __DIR__ . '/fixtures/certs/invalid_file.csr';
+
+        $environment = new Environment($username, $password, $sslCert, $sslKey, $sslPass);
+    }
+
+    protected function username() : string
+    {
+        return $this->randomKey();
+    }
+
+    protected function password() : string
+    {
+        return $this->randomKey();
+    }
+
+    protected function sslPassword() : string
+    {
+        return $this->randomKey();
+    }
+
+    protected function randomKey() : string
+    {
+        return bin2hex(random_bytes(16));
     }
 }
