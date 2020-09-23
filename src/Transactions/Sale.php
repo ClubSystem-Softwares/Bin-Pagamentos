fc4eaa8876b1e83ecdcc565788767ebdd6a6c9a6
Matheus Lopes Santos <fale_com_lopez@hotmail.com>
2020-09-22 22:27:10 -0300

    Inclusão da descrição dos dados que serão utilizados em uma sale

--- src/Transactions/Sale.php
+++ src/Transactions/Sale.php
@@ -4,21 +4,56 @@ namespace CSWeb\BIN\Transactions;
 
 use RuntimeException;
 
+/**
+ * Sale
+ *
+ * @author Matheus Lopes Santos <fale_com_lopez@hotmail.com>
+ * @version 1.0.0
+ * @package CSWeb\BIN\Transactions
+ */
 class Sale extends AbstractTransaction
 {
+    protected $data;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        //CreditCardTxType/
+            //Type
+
+        //CreditCardData/
+            // CardNumber
+            // ExpMonth
+            // ExpYear
+            // CardCodeValue
+
+        //Payment
+            //numberOfInstallments
+            //ChargeTotal
+            //Currency
+
+        //TransactionDetails
+            //OrderId
+    }
+
+    public function getRootNamespace(): string
+    {
+        return 'v1:Transaction';
+    }
+
     public function transformData()
     {
-        $dom = $this->soap;
-
-        $transaction = $dom->getElementsByTagName($this->getRootNamespace())->item(0);
+        $transaction = $this->soap->getElementsByTagName($this->getRootNamespace())
+                                  ->item(0);
 
         if (is_null($transaction)) {
             throw new RuntimeException('An error occurred during DOM\'s element search');
         }
 
-        $ccType = $dom->createElement('v1:CreditCardTxType');
-        $store  = $dom->createElement('v1:StoreId', 2724189910);
-        $type   = $dom->createElement('v1:Type', 'sale');
+        $ccType = $this->soap->createElement('v1:CreditCardTxType');
+        $store  = $this->soap->createElement('v1:StoreId', 2724189910);
+        $type   = $this->soap->createElement('v1:Type', 'sale');
 
         $ccType->appendChild($store);
         $ccType->appendChild($type);
@@ -26,10 +61,5 @@ class Sale extends AbstractTransaction
         $transaction->appendChild($ccType);
 
         return $this;
-    }
-
-    public function getRootNamespace(): string
-    {
-        return 'v1:Transaction';
     }
 }
