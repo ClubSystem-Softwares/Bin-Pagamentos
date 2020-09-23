5d50a74bf7bc5c5fd785821cb8a270bfccf45828
Matheus Lopes Santos <fale_com_lopez@hotmail.com>
2020-09-23 17:28:34 -0300

    Add TransactionDetail model

---
+++ src/Models/TransactionDetail.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace CSWeb\BIN\Models;
+
+/**
+ * TransactionDetail
+ *
+ * @author Matheus Lopes Santos <fale_com_lopez@hotmail.com>
+ * @version 1.0.0
+ * @package CSWeb\BIN\Models
+ */
+class TransactionDetail extends Model
+{
+    protected $prefix = 'v1';
+
+    protected $namespace = 'TransactionDetails';
+
+    protected $fillable = [
+        'OrderId'
+    ];
+}
