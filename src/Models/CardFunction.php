8ee128dab1999c736f2f8f16b3da61477d726583
Matheus Lopes Santos <fale_com_lopez@hotmail.com>
2020-10-06 21:37:57 -0300

    Add CardFunction model

---
+++ src/Models/CardFunction.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace CSWeb\BIN\Models;
+
+/**
+ * CardFunction
+ *
+ * @author Matheus Lopes Santos <fale_com_lopez@hotmail.com>
+ * @version 1.0.0
+ * @package CSWeb\BIN\Models
+ */
+class CardFunction extends Model
+{
+    protected $prefix = 'v1';
+
+    protected $namespace = 'cardFunction';
+
+    protected $fillable = [
+        'Type'
+    ];
+}
